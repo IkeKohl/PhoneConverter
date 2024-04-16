c5def3309f73331e918c7608ec45a5a2bf58e2f2
IsaacKohl <ikohl@wisc.edu>
2024-04-16 13:06:23 -0500

    Finished it all

--- PhoneConverter.java
+++ PhoneConverter.java
@@ -19,23 +19,62 @@ public class PhoneConverter {
         return num;
     }
 
-    public static int convertToNum(String grossNum) {
+    public static int letterToString(char let) {
+        
+        let = Character.toLowerCase(let);
+        
+        switch(let) {
+            case 'a','b','c':
+                return 2;
+            case 'd','e','f':
+                return 3;
+            case 'g','h','i':
+                return 4;
+            case 'j','k','l':
+                return 5;
+            case 'm','n','o':
+                return 6;
+            case 'p','q','r','s':
+                return 7;
+            case 't','u','v':
+                return 8;
+            case 'w','x','y','z':
+                return 9;
+        }
+        
+        return 1;
+    }
+
+    public static long convertToNum(String grossNum) {
         for (int i = 0; i < 11; i++) {
             char val = grossNum.charAt(i);
             if (Character.isDigit(val)) {
                 continue;
             }
             else if(Character.isLetter(val)) {
-                String newStr = grossNum.substring(0, i - 1) + 'i' + grossNum.substring(i + 1, grossNum.length());
+                int letter = letterToString(grossNum.charAt(i));
+                String ah = Integer.toString(letter);
+                String newStr = grossNum.substring(0, i) + "" + ah + "" + grossNum.substring(i + 1, grossNum.length());
                 grossNum = newStr;
             }
+            else {
+                System.out.println(grossNum.charAt(i) + " Character not allowed, please try again");
+                grossNum = "0";
+                break;
+            }
         }
-        return 0;
+        long finalVal = Long.parseLong(grossNum);
+        return finalVal;
     }
     public static void main(String[] args) {
         String num = getNumber();
-        
+        long finalVal = convertToNum(num);
+        if (finalVal == 0) {
 
+        }
+        else {
+            System.out.println(finalVal);
+        }
     }
 
 
